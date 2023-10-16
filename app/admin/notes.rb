6935111ebf75cc6812d0d51fa9e8eb046385fef8
waffer sky <waffer@example.com>
2023-10-16 14:45:31 +0530

    Add notes and subjecj file

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -10,41 +10,24 @@ ActiveAdmin.register Note do
   form.actions
 end
 
-show title: 'Your Note' do
-  h1 link_to note.title, admin_notes_path
-  h4 link_to note.subject.name,admin_subject_path(note.subject)
-  div(class: 'note-body') do
-    raw note.body
+  show title: 'Your Note' do
+    h1 link_to note.title, admin_notes_path
+    h4 link_to note.subject.name,admin_subject_path(note.subject)
+    div(class: 'note-body') do
+      raw note.body
+    end
   end
-end
 
-index do 
-  selectable_column
-  column :subject
-  column :title do |note|
-    link_to note.title,admin_note_path(note)
+  index do 
+    selectable_column
+    column :subject
+    column :title do |note|
+      link_to note.title,admin_note_path(note)
+    end
+    column :body do |note|
+      raw note.body.truncate_words(25)
+    end
+    column :created_at
+    column :updated_at
+   end
   end
-  column :body do |note|
-    raw note.body.truncate_words(25)
-  end
-  column :created_at
-  column :updated_at
-end
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :body, :subject_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :body, :subject_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
-end
